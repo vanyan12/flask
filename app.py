fedbe731b6966e51fc646b77fe2e60932d3f6502
vanyan12 <arenvanyan05@gmail.com>
2024-08-23 02:26:02 +0400

    a

--- app.py
+++ app.py
@@ -6,9 +6,6 @@ app = Flask(__name__)
 def home():
     return "Hello, World!"
 
-@app.route("/about")
-def about():
-    return "This is the About page."
 
 if __name__ == "__main__":
     app.run(debug=True)
