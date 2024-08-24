0da133ccb0eb97614951f303cb9a3598d8648b97
vanyan12 <arenvanyan05@gmail.com>
2024-08-24 17:26:21 +0400

    .

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Hel"
+
+@app.route("/hello")
+def hel():
+    return "hello"
 
 
 if __name__ == "__main__":
